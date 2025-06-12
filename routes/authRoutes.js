06f02d742391db36c5a54c3da8aba88fa8106f4d
CodingWithBars <barrojohnnems01@gmail.com>
2025-06-12 17:57:15 +0800

    mali
    update na

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { signup, login } = require('../controllers/authController');
+const { signup, login } = require('../controllers/authControllers');
 const router = express.Router();
 
 router.post('/signup', signup);
