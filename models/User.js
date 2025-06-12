84902f7147f93e46fc6456186f2a4f42c88fb9bb
CodingWithBars <barrojohnnems01@gmail.com>
2025-06-13 06:54:16 +0800

    change schema
    new

--- models/User.js
+++ models/User.js
@@ -1,10 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// Only store raw GPS coordinates
 const geoSchema = new mongoose.Schema({
-  municipality: { type: String, required: false }, // Optional, since you may or may not reverse geocode
-  barangay: { type: String, required: false },
-  street: { type: String, required: false },
   latitude: { type: Number, required: true },
   longitude: { type: Number, required: true },
 });
@@ -13,15 +11,17 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true },
-  geoLocation: geoSchema,
+  geoLocation: geoSchema, // Now only contains lat/lon
 });
 
+// Hash password before saving
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12);
   next();
 });
 
+// Compare password method
 userSchema.methods.comparePassword = async function (candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
