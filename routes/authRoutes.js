6383fb9cd8c015a0765f54149a601fb67aaf06d8
CodingWithBars <barrojohnnems01@gmail.com>
2025-06-20 07:08:19 +0800

    fix auth
    auth

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { signup, login, updateUser, deleteUser } = require('../controllers/authControllers');
-const authMiddleware = require('../middlewares/authMiddleware'); // your JWT middleware
+const auth = require('../middlewares/authMiddleware'); // your JWT middleware
 const router = express.Router();
 
 // Public routes
@@ -8,7 +8,7 @@ router.post('/signup', signup);
 router.post('/login', login);
 
 // Protected routes
-router.put('/user', authMiddleware, updateUser);
-router.delete('/user', authMiddleware, deleteUser);
+router.put('/user', auth, updateUser);
+router.delete('/user', auth, deleteUser);
 
 module.exports = router;
