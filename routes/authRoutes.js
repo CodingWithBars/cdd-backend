6975ba7787ac7d96830e8857d7b9d0ac4c130a60
CodingWithBars <barrojohnnems01@gmail.com>
2025-06-20 07:10:40 +0800

    test
    test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,26 @@
 const express = require('express');
-const { signup, login, updateUser, deleteUser } = require('../controllers/authControllers');
-const auth = require('../middlewares/authMiddleware'); // your JWT middleware
+const { signup, login } = require('../controllers/authControllers');
 const router = express.Router();
 
-// Public routes
 router.post('/signup', signup);
 router.post('/login', login);
 
-// Protected routes
-router.put('/user', auth, updateUser);
-router.delete('/user', auth, deleteUser);
-
 module.exports = router;
+
+
+
+
+// const express = require('express');
+// const { signup, login, updateUser, deleteUser } = require('../controllers/authControllers');
+// const auth = require('../middlewares/authMiddleware'); // your JWT middleware
+// const router = express.Router();
+
+// // Public routes
+// router.post('/signup', signup);
+// router.post('/login', login);
+
+// // Protected routes
+// router.put('/user', auth, updateUser);
+// router.delete('/user', auth, deleteUser);
+
+// module.exports = router;
